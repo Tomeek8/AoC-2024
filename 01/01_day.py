0a026cd02d20bfa6200e67bd96ae3e2589106d2b
Tomeek8 <77841767+Tomeek8@users.noreply.github.com>
2024-12-01 15:34:17 +0100

    Add files via upload

--- 01/01_day.py
+++ 01/01_day.py
@@ -1,1 +1,12 @@
+with open("input.txt") as f:
+    lines = f.read().splitlines()
 
+column_1 = sorted((int(x.split()[0]) for x in lines))
+column_2 = sorted((int(x.split()[1]) for x in lines))
+
+result_1 = sum(abs(x - y) for x, y in zip(column_1, column_2))
+print(result_1)
+
+
+result_2 = sum(num * column_2.count(num) for num in column_1)
+print(result_2)
