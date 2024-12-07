adcb6710bb7ffcbbbd6169d003d3301fa626fcf4
Tomeek8 <77841767+Tomeek8@users.noreply.github.com>
2024-12-07 23:26:23 +0100

    Update 07_day.py

--- 07/07_day.py
+++ 07/07_day.py
@@ -1,3 +1,5 @@
+import time
+
 def parse(path):
     with open(path) as f:
         return [
@@ -14,10 +16,13 @@ def is_equation_valid(test_value, numbers, part=1):
     for num in numbers[1:]:
         new_partial_sums = []
         for partial_sum in partial_sums:
-            new_partial_sums.append(partial_sum + num)
-            new_partial_sums.append(partial_sum * num)
+            if (np1:=partial_sum + num) <= test_value:
+                new_partial_sums.append(np1)
+            if (np2:=partial_sum * num) <= test_value:
+                new_partial_sums.append(np2)
             if part == 2:
-                new_partial_sums.append(int(f"{partial_sum}{num}"))
+                if (np3:=int(f"{partial_sum}{num}")) <= test_value:
+                    new_partial_sums.append(np3)
         partial_sums = new_partial_sums
     return test_value in partial_sums
 
@@ -30,6 +35,8 @@ def get_result(part):
             test_values_passed.append(test_value)
     return sum(test_values_passed)
 
-
+start_time = time.time()
 print(f"Part 1 result: {get_result(part=1)}")
 print(f"Part 2 result: {get_result(part=2)}")
+end_time = time.time()
+print(f"Time: {end_time - start_time:.3f} s")
