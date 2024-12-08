124e486758bb64ca9e2e2d262111e8d0a13ae9ce
Tomeek8 <77841767+Tomeek8@users.noreply.github.com>
2024-12-08 23:59:30 +0100

    Update 08_day.py

--- 08/08_day.py
+++ 08/08_day.py
@@ -8,9 +8,9 @@ def parse(path):
         return [list(line.strip()) for line in f]
 
 
-def is_inside(position, matrix):
+def is_inside(position, r_max, c_max):
     r, c = position
-    return 0 <= r < len(matrix) and 0 <= c < len(matrix[0])
+    return 0 <= r < r_max and 0 <= c < c_max
 
 
 def antennas_positions(matrix):
@@ -22,26 +22,60 @@ def antennas_positions(matrix):
     return ap
 
 
-def get_antinode_positions(pos1, pos2):
+def antinode_generator(pos1, pos2):
     vector = (pos2[0] - pos1[0], pos2[1] - pos1[1])
-    an1 = (pos1[0] - vector[0], pos1[1] - vector[1])
-    an2 = (pos2[0] + vector[0], pos2[1] + vector[1])
-    return an1, an2
+    while True:
+        antinode_position = (pos2[0] + vector[0], pos2[1] + vector[1])
+        yield antinode_position
+        pos2 = antinode_position
 
 
-def part1():
+def get_antinode_positions(pos1, pos2, r_max, c_max, multinodes_allowed=False):
+    antinodes = set()
+    gen1 = antinode_generator(pos1, pos2)
+    gen2 = antinode_generator(pos2, pos1)
+    if not multinodes_allowed:
+        an1 = next(gen1)
+        an2 = next(gen2)
+        if is_inside(an1, r_max, c_max):
+            antinodes.add(an1)
+        if is_inside(an2, r_max, c_max):
+            antinodes.add(an2)
+        return antinodes
+    while True:
+        an1 = next(gen1)
+        if is_inside(an1, r_max, c_max):
+            antinodes.add(an1)
+        else:
+            break
+    while True:
+        an2 = next(gen2)
+        if is_inside(an2, r_max, c_max):
+            antinodes.add(an2)
+        else:
+            break
+    return antinodes
+
+
+def main(multinodes_allowed=False):
     matrix = parse("./08/input.txt")
-    antinodes_positions = set()
-    for antenna, positions in antennas_positions(matrix).items():
+    r_max, c_max = len(matrix), len(matrix[0])
+    antinodes = set()
+    for positions in antennas_positions(matrix).values():
         for combination in combinations(positions, 2):
-            an1, an2 = get_antinode_positions(combination[0], combination[1])
-            if is_inside(an1, matrix):
-                antinodes_positions.add(an1)
-            if is_inside(an2, matrix):
-                antinodes_positions.add(an2)
-    return len(antinodes_positions)
+            pos1, pos2 = combination
+            antinodes.update(
+                get_antinode_positions(pos1, pos2, r_max, c_max, multinodes_allowed)
+            )
+    if not multinodes_allowed:
+        return len(antinodes)
+    for positions in antennas_positions(matrix).values():
+        if len(positions) > 1:
+            antinodes.update(positions)
+    return len(antinodes)
 
 
 start_time = time.time()
-print(f"Part 1 result: {part1()}")
+print(f"Part 1 result: {main()}")
+print(f"Part 2 result: {main(multinodes_allowed=True) }")
 print(f"Time: {time.time() - start_time:.3f} s")
